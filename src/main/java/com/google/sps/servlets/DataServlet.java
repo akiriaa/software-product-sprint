238e1a64136177195914a1d6213fcbe07a71a0e2
Akiria Anderson <akiria.anderson@gmail.com>
2020-06-20 19:42:28 -0500

    Servlet returns login status

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,114 +14,105 @@
 
 package com.google.sps.servlets;
 
-import com.google.gson.Gson;
-import java.io.IOException;
-import java.util.Date;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
-import java.util.Enumeration;
-import java.lang.String;
-//import com.google.sps.data.Task;
+import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.Task;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.List;
+import java.lang.String;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-    ArrayList<String> messages = new ArrayList<String>();
-    //DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    private ArrayList<String> tasks = new ArrayList<String>();
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Query query = new Query("Message").addSort("message-container", SortDirection.DESCENDING);
-        
-        Gson gson = new Gson();
-        String json = gson.toJson(messages);
+        String json = convertToJson(tasks);
+        Query query = new Query("Task").addSort("timestamp", SortDirection.DESCENDING);
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         PreparedQuery results = datastore.prepare(query);
 
-        List<String> messages = new ArrayList<>();
+   
+        List<Task> tasks = new ArrayList<>();
         for (Entity entity : results.asIterable()) {
-            //long id = messageEnt.getKey().getId();
-            String message = (String) entity.getProperty("message-container");
-            //long timestamp = (long) messageEnt.getProperty("timestamp");
-            response.getOutputStream().println(message);
-            //String messages = new Message(message-container);
-            //messages.add(message);
+            long id = entity.getKey().getId();
+            String name = (String) entity.getProperty("name");
+            String comment = (String) entity.getProperty("comment");
+            long timestamp = (long) entity.getProperty("timestamp");
+            Task task = new Task(id, name, comment, timestamp);
+            tasks.add(task);
         }
-    //          response.setContentType("application/json;");
-
-
-        // Query query = new Query("Task").addSort("timestamp", SortDirection.DESCENDING);
-
-        // DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        // PreparedQuery results = datastore.prepare(query);
-
-        // List<Task> tasks = new ArrayList<>();
-        // for (Entity entity : results.asIterable()) {
-        //   long id = entity.getKey().getId();
-        //   String title = (String) entity.getProperty("title");
-        //   long timestamp = (long) entity.getProperty("timestamp");
-
-        //   Task task = new Task(id, title, timestamp);
-        //   tasks.add(task);
-    // }
-        // Gson gson = new Gson();
-      
-        // String json = convertToJson(messages);
-
-        response.setContentType("text/html;");
-        response.getWriter().println(json);
-        //printRequest(response.getWriter(), request);
-    }
-
-    private String convertToJson(ArrayList<String> messages){
         Gson gson = new Gson();
-        String json = gson.toJson(messages);
+        response.setContentType("application/json;");
+        response.getWriter().println(gson.toJson(tasks));
+    
+        UserService userService = UserServiceFactory.getUserService();
+        if (userService.isUserLoggedIn()) {
+            String userEmail = userService.getCurrentUser().getEmail();
+            String urlToRedirectToAfterUserLogsOut = "/";
+            String logoutUrl = userService.createLogoutURL(urlToRedirectToAfterUserLogsOut);
+
+            response.getWriter().println("<p>Hello " + userEmail + "!</p>");
+            response.getWriter().println("<p>Logout <a href=\"" + logoutUrl + "\">here</a>.</p>");
+        }
+        else {
+            String urlToRedirectToAfterUserLogsIn = "/";
+            String loginUrl = userService.createLoginURL(urlToRedirectToAfterUserLogsIn);
+
+            response.getWriter().println("<p>Hello stranger.</p>");
+            response.getWriter().println("<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>");
+        }
+    }
+ 
+    private String convertToJson(ArrayList message){
+        Gson gson = new Gson();
+        String json = gson.toJson(message);
         return json;
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // Get the input from the form.
-        String message = getMessage(request);
-        //String message = request.getMessage("message-container");
-        response.setContentType("text/html;"); //
-        //System.out.println("do post message");
-        messages.add(message);
+        UserService userService = UserServiceFactory.getUserService();
 
-        Entity messageEnt = new Entity("message");
-        messageEnt.setProperty("message-container", message);
-
+        // Only logged-in users can post messages
+        if (!userService.isUserLoggedIn()) {
+            response.sendRedirect("/shoutbox");
+            return;
+        }
+        
+        String name = request.getParameter("name");
+        String comment = request.getParameter("comment");
+        long timestamp = System.currentTimeMillis();
+        Entity taskEntity = new Entity("Task");
+        taskEntity.setProperty("name", name);
+        // taskEntity.setProperty("Email: ", email);
+        taskEntity.setProperty("comment", comment);
+        taskEntity.setProperty("timestamp", timestamp);
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        datastore.put(messageEnt);
-        //redirect
-        response.sendRedirect("/index.html");
-
+        datastore.put(taskEntity);
+        response.sendRedirect("/contact.html");
     }
 
-    private String getMessage(HttpServletRequest request){ //, String name, String defaultValue
-        String message = request.getParameter("message-container");
-        return message;
-    }
 }
-    
-   
-    // private String convertToJsonUsingGson(DataServlet messages) {
-    //     Gson gson = new Gson();
-    //     String json = gson.toJson(messages);
-    //     return json;
-    // }
+
 
 
